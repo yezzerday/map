56ab0b2a21596e45fae1e55b64f728904da58d49
yezzerday <107908675+yezzerday@users.noreply.github.com>
2024-10-14 08:35:27 +0700

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 8th Wall, Inc.
-//
-// app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
-// is loaded, and before body.html is loaded.
-
-import {gltfMorphComponent, morphTargetDragComponent} from './morph-target-drag'
-
-AFRAME.registerComponent('gltf-morph', gltfMorphComponent)
-AFRAME.registerComponent('morph-target-drag', morphTargetDragComponent)
